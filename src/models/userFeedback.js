c57f17a10cd2368006018d567292db6d203264a3
Joe <dygood@outlook.com>
2019-01-14 10:54:11 +0800

    修改文件后缀,调整代码

---
+++ src/models/userFeedback.js
@@ -0,0 +1,154 @@
+import modelExtend from "dva-model-extend";
+import {
+  feedbackListApi,
+  deviceProductListApi,
+  feedbackUpdateApi
+} from "../services/api";
+import queryString from "query-string";
+import { message } from "antd";
+import $ from "jquery";
+
+export default {
+  namespace: "userFeedback",
+  state: {
+    feedbackData: [],
+    deviceProductListData: [],
+    pagination: {
+      total: 0,
+      pageSize: 0,
+      current: 0,
+      pageCount: 0
+    }, //分页数据
+    searchList: {}, //查询条件
+    pageindex: 0, //分页开始 第几页
+    pagesize: 10 //返回条数
+  },
+  subscriptions: {
+    setup({ dispatch, history }) {
+      history.listen(location => {
+        //页面初始化执行
+        if (location.pathname === "/userFeedback") {
+          let _ars = {
+            userToken: localStorage.getItem("userToken"),
+            endTime: null,
+            firstRow: null,
+            isProcessed: null,
+            pageNum: 0,
+            pageRows: 10,
+            productId: null,
+            startTime: null
+          };
+          dispatch({ type: "queryFeedbackListData", payload: _ars });
+          dispatch({ type: "queryDeviceProductListData" });
+        }
+      });
+    }
+  },
+
+  effects: {
+    *queryFeedbackListData({ payload }, { call, put }) {
+      const data = yield call(feedbackListApi, payload);
+      if (data.code == 0) {
+        let result = data.data;
+        let _pag = {};
+        _pag.total =
+          typeof result.totalRows == "undefined" ? 0 : result.totalRows;
+        _pag.pageSize =
+          typeof result.pageRows == "undefined" ? 0 : result.pageRows;
+        _pag.current =
+          typeof result.pageNum == "undefined" ? 0 : result.pageNum;
+        if (
+          typeof result.totalRows == "undefined" ||
+          typeof result.pageRows == "undefined"
+        )
+          _pag.pageCount = 0;
+        else
+          _pag.pageCount =
+            parseInt((result.totalRows - 1) / result.pageRows) + 1;
+        let feedbacks = result.feedbacks;
+        let feedbackData = [];
+        for (var i = 0; i < feedbacks.length; i++) {
+          feedbackData[i] = {
+            description: feedbacks[i].description,
+            mobileAndNickname: {
+              mobile: feedbacks[i].mobile,
+              nickname: feedbacks[i].nickname
+            },
+            productName: feedbacks[i].productName,
+            createTime: feedbacks[i].createTime,
+            feedbackId: feedbacks[i].feedbackId,
+            isProcessed: feedbacks[i].isProcessed
+          };
+        }
+        yield put({
+          type: "queryFeedbackListDataSuccess",
+          payload: feedbackData,
+          page: _pag
+        });
+      } else {
+        message.error("获取数据失败,错误信息:" + data.msg);
+      }
+    },
+    *queryDeviceProductListData({ payload }, { call, put }) {
+      const prams = { userToken: localStorage.getItem("userToken") };
+      const dataDeviceProductList = yield call(deviceProductListApi, prams);
+      if (dataDeviceProductList.code == 0) {
+        yield put({
+          type: "queryDeviceProductListDataSuccess",
+          payload: dataDeviceProductList.data
+        });
+      } else {
+        message.error(
+          "获取产品列表数据失败,错误信息:" + dataDeviceProductList.msg
+        );
+      }
+    },
+    *updateFeedback({ payload }, { call, put }) {
+      const update = yield call(feedbackUpdateApi, payload);
+      if (update.code == 0) {
+        message.info("更新成功:" + update.msg);
+      } else {
+        message.error("更新失败:" + update.msg);
+      }
+    }
+  },
+  reducers: {
+    clearData(state) {
+      return {
+        ...state,
+        feedbackData: [],
+        deviceProductListData: [],
+        pagination: {},
+        searchList: {},
+        pageindex: 0,
+        pagesize: 10
+      }; //分页数据 //查询条件 //分页开始 第几页 //返回条数
+    },
+    //返回数据列表
+    queryFeedbackListDataSuccess(state, action) {
+      return {
+        ...state,
+        feedbackData: action.payload,
+        pagination: action.page
+      };
+    },
+    queryDeviceProductListDataSuccess(state, action) {
+      return { ...state, deviceProductListData: action.payload };
+    },
+    //分页参数
+    setPage(state, action) {
+      return {
+        ...state,
+        pageindex: action.payload,
+        pagesize: action.pageSize
+      };
+    },
+    //查询条件
+    searchList(state, action) {
+      return {
+        ...state,
+        searchList: action.payload
+      };
+    }
+  }
+};
---
+++ src/routes/activeData/activeData.js
@@ -0,0 +1,267 @@
+import React, { PureComponent, Fragment } from "react";
+import { connect } from "dva";
+import moment from "moment";
+import { Table, Tabs, Icon, Row, Col, Card, Form, Select, message } from "antd";
+import styles from "./activeData.less";
+import $ from "jquery";
+import classnames from "classnames";
+
+const ReactHighcharts = require("react-highcharts");
+const FormItem = Form.Item;
+const { Option } = Select;
+const TabPane = Tabs.TabPane;
+
+const ActiveData = ({
+  activeData,
+  loading,
+  dispatch,
+  formValues, //搜索条件
+  form: {
+    getFieldDecorator,
+    validateFieldsAndScroll,
+    setFieldsValue,
+    getFieldsValue
+  }
+}) => {
+  let {
+    activateSummaryData,
+    activateData,
+    deviceProductListData,
+    selected
+  } = activeData;
+  //查询条件
+  let productID = 0;
+  //查询条件
+  const handleChange = e => {
+    productID = e;
+    let activate = {
+      userToken: localStorage.getItem("userToken"),
+      period: 7,
+      productId: e
+    };
+    //赛选数据
+    dispatch({ type: "activeData/queryActivateSummary", payload: activate });
+    dispatch({ type: "activeData/queryActivate", payload: activate });
+    dispatch({ type: "activeData/selected", payload: 7 });
+  };
+
+  let dailyConfig = {
+    chart: { height: 450 },
+    xAxis: { categories: activateData.dateArray },
+    yAxis: {
+      title: { text: "激活设备/个" },
+      plotLines: [{ value: 0, width: 1, color: "#808080" }]
+    },
+    title: { text: null },
+    legend: { enabled: false },
+    credits: { enabled: false },
+    series: [{ name: "激活设备/个", data: activateData.numArray }]
+  }; // 隐藏右下角版权
+
+  let totalConfig = {
+    chart: { height: 450 },
+    xAxis: { categories: activateData.dateArray },
+    yAxis: {
+      title: { text: "激活设备/个" },
+      plotLines: [{ value: 0, width: 1, color: "#808080" }]
+    },
+    title: { text: null },
+    legend: { enabled: false },
+    credits: { enabled: false }, // 隐藏右下角版权
+    series: [{ name: "激活设备/个", data: activateData.totalArray }]
+  };
+
+  //定义表头
+  const columns = [
+    {
+      title: "日期",
+      dataIndex: "actDate",
+      render: (text, record) => {
+        return <div>{record.actDate}</div>;
+      }
+    },
+    {
+      title: "每日激活数量",
+      dataIndex: "num",
+      render: (text, record) => {
+        return <div>{record.num}</div>;
+      }
+    },
+    {
+      title: "累计激活总数",
+      dataIndex: "total",
+      render: (text, record) => {
+        return <div>{record.total}</div>;
+      }
+    }
+  ];
+
+  const getData = k => {
+    dispatch({ type: "activeData/selected", payload: k });
+    let activate = {
+      userToken: localStorage.getItem("userToken"),
+      period: k,
+      productId: productID
+    };
+    dispatch({ type: "activeData/queryActivate", payload: activate });
+  };
+
+  return (
+    <div>
+      <Card>
+        <div className={styles.tableList}>
+          <div className={styles.tableListForm}>
+            <Form layout="inline">
+              <Row gutter={{ md: 8, lg: 24, xl: 48 }}>
+                <Col md={8} sm={24}>
+                  <FormItem label="产品" style={{ marginLeft: 30 }}>
+                    {getFieldDecorator("productId", {
+                      initialValue: "全部"
+                    })(
+                      <Select
+                        placeholder="全部"
+                        onChange={handleChange}
+                        style={{ width: "100%" }}
+                      >
+                        <Option value={0}>全部</Option>
+                        {deviceProductListData.map(product => (
+                          <Option value={product.productId}>
+                            {product.productName}
+                          </Option>
+                        ))}
+                      </Select>
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+        </div>
+      </Card>
+
+      <div style={{ marginTop: "15px" }} className={styles.indexTop}>
+        <Card className={styles.indexTopL}>
+          <div className={styles.indexCont}>
+            <div
+              className={styles.indexCont_span}
+              style={{ marginRight: "10%" }}
+            >
+              <span className={styles.indexTop_text}>今日激活</span>
+              <span style={{ color: "#1890FF" }}>
+                {activateSummaryData.todayActivate}&nbsp;
+              </span>
+              <div className={styles.indexBottom_text}>
+                <span>昨日激活&nbsp;&nbsp;</span>
+                <span>{activateSummaryData.yesterdayActivate}&nbsp;</span>
+              </div>
+            </div>
+            <div
+              className={styles.indexCont_span}
+              style={{ marginRight: "10%" }}
+            >
+              <span className={styles.indexTop_text}>近7日激活</span>
+              <span style={{ color: "#1890FF" }}>
+                {activateSummaryData.periodActivate}&nbsp;
+              </span>
+              <div className={styles.indexBottom_text}>
+                <span>上7日激活&nbsp;&nbsp;</span>
+                <span>{activateSummaryData.prePeriodActivate}&nbsp;</span>
+              </div>
+            </div>
+            <div
+              className={styles.indexCont_span}
+              style={{ marginTop: "-48px", marginRight: "10%" }}
+            >
+              <span className={styles.indexTop_text}>累计激活总数</span>
+              <span style={{ color: "#1890FF" }}>
+                {activateSummaryData.totalActivate}&nbsp;
+              </span>
+            </div>
+          </div>
+        </Card>
+      </div>
+
+      <Card style={{ marginTop: "15px" }}>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="每日激活趋势" key="1">
+            <div className={styles.indexData}>
+              <div className={styles.indexData_top}>
+                <ul
+                  className={styles.indexData_topUL}
+                  style={{ float: "right" }}
+                >
+                  <li
+                    className={selected == 7 ? styles.active : ""}
+                    onClick={getData.bind(this, 7)}
+                  >
+                    近7天
+                  </li>
+                  <li
+                    className={selected == 15 ? styles.active : ""}
+                    onClick={getData.bind(this, 15)}
+                  >
+                    近15天
+                  </li>
+                  <li
+                    className={selected == 30 ? styles.active : ""}
+                    onClick={getData.bind(this, 30)}
+                  >
+                    近30天
+                  </li>
+                </ul>
+                <div style={{ width: "100%" }}>
+                  <ReactHighcharts config={dailyConfig} />
+                </div>
+              </div>
+            </div>
+          </TabPane>
+          <TabPane tab="累计激活趋势" key="2">
+            <div className={styles.indexData}>
+              <div className={styles.indexData_top}>
+                <ul
+                  className={styles.indexData_topUL}
+                  style={{ float: "right" }}
+                >
+                  <li
+                    className={selected == 7 ? styles.active : ""}
+                    onClick={getData.bind(this, 7)}
+                  >
+                    近7天
+                  </li>
+                  <li
+                    className={selected == 15 ? styles.active : ""}
+                    onClick={getData.bind(this, 15)}
+                  >
+                    近15天
+                  </li>
+                  <li
+                    className={selected == 30 ? styles.active : ""}
+                    onClick={getData.bind(this, 30)}
+                  >
+                    近30天
+                  </li>
+                </ul>
+                <div style={{ width: "100%" }}>
+                  <ReactHighcharts config={totalConfig} />
+                </div>
+              </div>
+            </div>
+          </TabPane>
+        </Tabs>
+      </Card>
+      <Card title="激活数据明细">
+        <Table
+          columns={columns}
+          dataSource={activateData.listArray}
+          bordered={false}
+          pagination={false}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default connect(({ activeData, loading }) => ({
+  activeData,
+  loading: loading.models.activeData
+}))(Form.create()(ActiveData));
