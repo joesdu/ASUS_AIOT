5ccf077101af2e04c4e112522af60fd58d8da8ba
Joe <dygood@outlook.com>
2019-06-19 09:30:47 +0800

    基本完成新增編輯頁面的交互.還有一些問題需要繼續調整

--- src/models/03RightsManagement/roleManage/roleAddEdit.js
+++ src/models/03RightsManagement/roleManage/roleAddEdit.js
@@ -4,20 +4,28 @@ import { message } from "antd";
 export default {
     namespace: "roleAddEdit",
     state: {
-        roleListData: [],
-        pagination: {
-            total: 0,
-            pageSize: 10,
-            current: 0,
-            pageCount: 0
-        }
+        defaultCheckData: [],
+        checkAll: false,
+        indeterminateCheckAll: false,
+        operationCheckAll: false,
+        indeterminateOperation: false,
+        valueOperation: [],
+        dataCheckAll: false,
+        indeterminateData: false,
+        valueData: [],
+        permissionsCheckAll: false,
+        indeterminatePermissions: false,
+        valuePermissions: [],
+        systemCheckAll: false,
+        indeterminateSystem: false,
+        valueSystem: []
     },
     subscriptions: {
         setup({ dispatch, history }) {
             history.listen(location => {
                 //页面初始化执行
                 if (location.pathname === "/roleAddEdit") {
-                    dispatch({ type: "getRoleList", payload: "1" });
+                    //dispatch({ type: "getRoleList", payload: "1" });
                 }
             });
         }
@@ -26,78 +34,75 @@ export default {
     effects: {
         *getRoleList({ payload }, { call, put }) {
             //const data = yield call(feedbackListApi, payload);
-            let data = {
-                code: 0,
-                data: {
-                    roleList: payload === "1" ? [{
-                        id: 0,
-                        name: "测试姓名0",
-                        states: true,
-                        createTime: new Date(),
-                        related: true
-                    }, {
-                        id: 1,
-                        name: "测试姓名1",
-                        states: false,
-                        createTime: new Date(),
-                        related: false
-                    }] : [{
-                        id: 0,
-                        name: "测试姓名0",
-                        states: true,
-                        createTime: new Date(),
-                        related: false
-                    }, {
-                        id: 1,
-                        name: "测试姓名1",
-                        states: true,
-                        createTime: new Date(),
-                        related: false
-                    }],
-                    pageNum: 0,
-                    pageRows: 10,
-                    totalRows: 1
-                },
-                msg: "沒有錯誤"
-            };
-            let _pag = {};
+
             if (data == null || data.length == 0 || data == {} || data.code != 0) {
                 message.error(data != null ? "获取数据失败,错误信息:" + data.msg : "获取数据失败");
-                yield put({ type: "getRoleListSuccess", payload: null, page: _pag });
+                yield put({ type: "getRoleListSuccess", payload: null });
             } else {
                 if (data.data == null || data.data == {} || data.data == undefined) {
                     message.info("无数据");
-                    yield put({ type: "getRoleListSuccess", payload: null, page: _pag });
+                    yield put({ type: "getRoleListSuccess", payload: null });
                 }
                 else {
-                    _pag.total = typeof data.data.totalRows == undefined ? 0 : data.data.totalRows;
-                    _pag.pageSize = typeof data.data.pageRows == undefined ? 0 : data.data.pageRows;
-                    _pag.current = typeof data.data.pageNum == undefined ? 0 : data.data.pageNum;
-                    if (typeof data.data.totalRows == undefined || typeof data.data.pageRows == undefined)
-                        _pag.pageCount = 0;
-                    else
-                        _pag.pageCount = parseInt((data.data.totalRows - 1) / data.data.pageRows) + 1;
-                    yield put({ type: "getRoleListSuccess", payload: data.data.roleList, page: _pag });
+                    yield put({ type: "getRoleListSuccess", payload: data.data.roleList });
                 }
             }
         },
     },
     reducers: {
-        clearData(state) {
+        setAll(state, action) {
             return {
                 ...state,
-                roleListData: [],
-                pagination: {
-                    total: 0,
-                    pageSize: 10,
-                    current: 0,
-                    pageCount: 0
-                }
-            };
+                valueOperation: action.payload.operation,
+                valueData: action.payload.data,
+                valuePermissions: action.payload.permissions,
+                valueSystem: action.payload.system,
+                indeterminateOperation: action.payload.indeterminate,
+                indeterminateData: action.payload.indeterminate,
+                indeterminatePermissions: action.payload.indeterminate,
+                indeterminateSystem: action.payload.indeterminate,
+                indeterminateCheckAll: action.payload.indeterminate,
+                checkAll: action.payload.checked,
+                operationCheckAll: action.payload.checked,
+                dataCheckAll: action.payload.checked,
+                permissionsCheckAll: action.payload.checked,
+                systemCheckAll: action.payload.checked
+            }
         },
-        //返回数据列表
+        setOperation(state, action) {
+            return {
+                ...state,
+                valueOperation: action.payload.checkList,
+                indeterminateOperation: action.payload.indeterminate,
+                operationCheckAll: action.payload.checked
+            }
+        },
+        setData(state, action) {
+            return {
+                ...state,
+                valueData: action.payload.checkList,
+                indeterminateData: action.payload.indeterminate,
+                dataCheckAll: action.payload.checked
+            }
+        },
+        setPermission(state, action) {
+            return {
+                ...state,
+                valuePermissions: action.payload.checkList,
+                indeterminatePermissions: action.payload.indeterminate,
+                permissionsCheckAll: action.payload.checked
+            }
+        },
+        setSystem(state, action) {
+            return {
+                ...state,
+                valueSystem: action.payload.checkList,
+                indeterminateSystem: action.payload.indeterminate,
+                systemCheckAll: action.payload.checked
+            }
+        },
         getRoleListSuccess(state, action) {
-            return { ...state, roleListData: action.payload, pagination: action.page };
+            return { ...state, roleListData: action.payload };
         },
         productListSuccess(state, action) {
             return { ...state, deviceProductListData: action.payload };
--- src/routes/03RightsManagement/roleManage/roleAddEdit.js
+++ src/routes/03RightsManagement/roleManage/roleAddEdit.js
@@ -13,18 +13,121 @@ const RoleAddEdit = ({
         getFieldsValue
     }
 }) => {
-    let { detailData } = roleAddEdit;
+    let { defaultCheckData, checkAll, indeterminateCheckAll,
+        operationCheckAll, indeterminateOperation, valueOperation,
+        dataCheckAll, indeterminateData, valueData,
+        permissionsCheckAll, indeterminatePermissions, valuePermissions,
+        systemCheckAll, indeterminateSystem, valueSystem
+    } = roleAddEdit;
 
-    const formItemLayout = {
-        labelCol: {
-            xs: { span: 24 },
-            sm: { span: 5 },
-        },
-        wrapperCol: {
-            xs: { span: 24 },
-            sm: { span: 6 },
-        },
-    };
+    const operationElementCount = 8;//運營中心權限個數
+    const operationElements = ["6", "14", "7", "17", "18", "8", "19", "20"];//運營中心權限ID
+    const dataElementCount = 4;//數據中心權限個數
+    const dataElements = ["9", "21", "10", "22"];//數據中心權限ID
+    const permissionsElementCount = 6;//權限控制權限個數
+    const permissionsElements = ["11", "23", "24", "12", "25", "26"];//權限控制權限ID
+    const systemElementCount = 3;//系統設置權限個數
+    const systemElements = ["13", "27", "28"];//系統設置權限ID
+    const allElementCount = operationElementCount + dataElementCount + permissionsElementCount + systemElementCount;
+
+    const getAllElementCount = () => {
+        return valueOperation.length + valueData.length + valuePermissions.length + valueSystem.length;
+    }
+
+    const checkAllOnChange = (e) => {
+        dispatch({
+            type: "roleAddEdit/setAll", payload: {
+                operation: e.target.checked ? operationElements : [],
+                data: e.target.checked ? dataElements : [],
+                permissions: e.target.checked ? permissionsElements : [],
+                system: e.target.checked ? systemElements : [],
+                indeterminate: false,
+                checked: e.target.checked,
+            }
+        });
+    }
+
+    //运营中心多选框中的元素被点击
+    const operationOnChange = (checkList) => {
+        dispatch({
+            type: "roleAddEdit/setOperation", payload: {
+                checkList: checkList,
+                indeterminate: !!checkList.length && checkList.length < operationElementCount,
+                checked: checkList.length === operationElementCount,
+            }
+        });
+    }
+    //运营中心全选控制
+    const operationAllOnChange = (e) => {
+        dispatch({
+            type: "roleAddEdit/setOperation", payload: {
+                checkList: e.target.checked ? operationElements : [],
+                indeterminate: false,
+                checked: e.target.checked,
+            }
+        });
+    }
+    //数据中心多选框中的元素被点击
+    const dataOnChange = (checkList) => {
+        dispatch({
+            type: "roleAddEdit/setData", payload: {
+                checkList: checkList,
+                indeterminate: !!checkList.length && checkList.length < dataElementCount,
+                checked: checkList.length === dataElementCount,
+            }
+        });
+    }
+    //數據中心全選控制
+    const dataAllOnChange = (e) => {
+        dispatch({
+            type: "roleAddEdit/setData", payload: {
+                checkList: e.target.checked ? dataElements : [],
+                indeterminate: false,
+                checked: e.target.checked,
+            }
+        });
+    }
+    //權限控制多选框中的元素被点击
+    const permissionsOnChange = (checkList) => {
+        dispatch({
+            type: "roleAddEdit/setPermission", payload: {
+                checkList: checkList,
+                indeterminate: !!checkList.length && checkList.length < permissionsElementCount,
+                checked: checkList.length === permissionsElementCount,
+            }
+        });
+    }
+    //權限控制全選控制
+    const permissionsAllOnChange = (e) => {
+        dispatch({
+            type: "roleAddEdit/setPermission", payload: {
+                checkList: e.target.checked ? permissionsElements : [],
+                indeterminate: false,
+                checked: e.target.checked,
+            }
+        });
+    }
+    //系統設置多选框中的元素被点击
+    const systemOnChange = (checkList) => {
+        dispatch({
+            type: "roleAddEdit/setSystem", payload: {
+                checkList: checkList,
+                indeterminate: !!checkList.length && checkList.length < systemElementCount,
+                checked: checkList.length === systemElementCount,
+            }
+        });
+    }
+    //系統設置全選控制
+    const systemAllOnChange = (e) => {
+        dispatch({
+            type: "roleAddEdit/setSystem", payload: {
+                checkList: e.target.checked ? systemElements : [],
+                indeterminate: false,
+                checked: e.target.checked,
+            }
+        });
+    }
+
 
     return (
         <div>
@@ -39,14 +142,10 @@ const RoleAddEdit = ({
                             )}
                         </Form.Item>
                         <Form.Item label="启用状态" style={{ marginLeft: 13 }}>
-                            {getFieldDecorator('states')(
-                                <Switch checkedChildren="开" unCheckedChildren="关" defaultChecked={true} />
-                            )}
+                            <Switch checkedChildren="开" unCheckedChildren="关" defaultChecked={true} />
                         </Form.Item>
                         <Form.Item label="权限设置" style={{ marginLeft: 13, marginBottom: 0 }}>
-                            {getFieldDecorator('captcha')(
-                                <Checkbox>全选</Checkbox>
-                            )}
+                            <Checkbox onChange={checkAllOnChange} indeterminate={indeterminateCheckAll} checked={checkAll}>全选</Checkbox>
                         </Form.Item>
                     </Form>
                 </div>
@@ -54,28 +153,28 @@ const RoleAddEdit = ({
 
             <Card title={
                 <div>
-                    <Checkbox style={{ fontSize: 16 }}>运营中心</Checkbox>
+                    <Checkbox style={{ fontSize: 16 }}
+                        value="2" onChange={operationAllOnChange}
+                        indeterminate={indeterminateOperation}
+                        checked={operationCheckAll}>运营中心</Checkbox>
                     <span style={{ marginLeft: 16, color: '#B4B4B4' }}>模块的所有功能未开启时，角色操作界面中不显示此模块</span>
                 </div>
             } style={{ marginTop: 20 }}>
                 <div className={styles.tableCheckGroup}>
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={8}><Checkbox value="A">设备管理</Checkbox></Col>
-                            <Col span={8}><Checkbox value="B">查看</Checkbox></Col>
+                    <Checkbox.Group onChange={operationOnChange} value={valueOperation}>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="6" style={{ fontSize: 15, fontStyle: Blob }}>设备管理</Checkbox></Col>
+                            <Col span={8}><Checkbox value="14">查看</Checkbox></Col>
                         </Row>
-                    </Checkbox.Group>
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={8}><Checkbox value="A">用户反馈</Checkbox></Col>
-                            <Col span={8}><Checkbox value="B">查看</Checkbox></Col>
-                            <Col span={8}><Checkbox value="C">标记</Checkbox></Col>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="7" style={{ fontSize: 15, fontStyle: Blob }}>用户反馈</Checkbox></Col>
+                            <Col span={8}><Checkbox value="17">查看</Checkbox></Col>
+                            <Col span={8}><Checkbox value="18">标记</Checkbox></Col>
                         </Row>
-                    </Checkbox.Group>
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={8}><Checkbox value="A">用户管理</Checkbox></Col>
-                            <Col span={8}><Checkbox value="B">查看</Checkbox></Col>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="8" style={{ fontSize: 15, fontStyle: Blob }}>用户管理</Checkbox></Col>
+                            <Col span={8}><Checkbox value="19">查询</Checkbox></Col>
+                            <Col span={8}><Checkbox value="20">查看详情</Checkbox></Col>
                         </Row>
                     </Checkbox.Group>
                 </div>
@@ -83,21 +182,23 @@ const RoleAddEdit = ({
 
             <Card title={
                 <div>
-                    <Checkbox style={{ fontSize: 16 }}>数据中心</Checkbox>
+                    <Checkbox style={{ fontSize: 16 }}
+                        value="3"
+                        onChange={dataAllOnChange}
+                        indeterminate={indeterminateData}
+                        checked={dataCheckAll}>数据中心</Checkbox>
                     <span style={{ marginLeft: 16, color: '#B4B4B4' }}>模块的所有功能未开启时，角色操作界面中不显示此模块</span>
                 </div>
             } style={{ marginTop: 20 }}>
                 <div className={styles.tableCheckGroup}>
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={8}><Checkbox value="A">激活数据</Checkbox></Col>
-                            <Col span={8}><Checkbox value="B">查看</Checkbox></Col>
+                    <Checkbox.Group onChange={dataOnChange} value={valueData}>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="9" style={{ fontSize: 15, fontStyle: Blob }}>激活数据</Checkbox></Col>
+                            <Col span={8}><Checkbox value="21">查看</Checkbox></Col>
                         </Row>
-                    </Checkbox.Group>
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={8}><Checkbox value="A">活跃数据</Checkbox></Col>
-                            <Col span={8}><Checkbox value="B">查看</Checkbox></Col>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="10" style={{ fontSize: 15, fontStyle: Blob }}>活跃数据</Checkbox></Col>
+                            <Col span={8}><Checkbox value="22">查看</Checkbox></Col>
                         </Row>
                     </Checkbox.Group>
                 </div>
@@ -105,36 +206,65 @@ const RoleAddEdit = ({
 
             <Card title={
                 <div>
-                    <Checkbox style={{ fontSize: 16 }}>权限管理</Checkbox>
+                    <Checkbox style={{ fontSize: 16 }}
+                        value="4"
+                        onChange={permissionsAllOnChange}
+                        indeterminate={indeterminatePermissions}
+                        checked={permissionsCheckAll}>权限管理</Checkbox>
                     <span style={{ marginLeft: 16, color: '#B4B4B4' }}>模块的所有功能未开启时，角色操作界面中不显示此模块</span>
                 </div>
             } style={{ marginTop: 20 }}>
                 <div className={styles.tableCheckGroup}>
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={8}><Checkbox value="A">人员管理</Checkbox></Col>
-                            <Col span={8}><Checkbox value="B">查看</Checkbox></Col>
-                            <Col span={8}><Checkbox value="C">管理</Checkbox></Col>
+                    <Checkbox.Group onChange={permissionsOnChange} value={valuePermissions}>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="11" style={{ fontSize: 15, fontStyle: Blob }}>人员管理</Checkbox></Col>
+                            <Col span={8}><Checkbox value="23">查看</Checkbox></Col>
+                            <Col span={8}><Checkbox value="24">管理</Checkbox></Col>
                         </Row>
-                    </Checkbox.Group>
-                    <Checkbox.Group>
-                        <Row>
-                            <Col span={8}><Checkbox value="A">角色管理</Checkbox></Col>
-                            <Col span={8}><Checkbox value="B">查看</Checkbox></Col>
-                            <Col span={8}><Checkbox value="C">管理</Checkbox></Col>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="12" style={{ fontSize: 15, fontStyle: Blob }}>角色管理</Checkbox></Col>
+                            <Col span={8}><Checkbox value="25">查看</Checkbox></Col>
+                            <Col span={8}><Checkbox value="26">管理</Checkbox></Col>
                         </Row>
                     </Checkbox.Group>
                 </div>
             </Card>
 
-            <Affix style={{ position: 'absolute', bottom: 0, right: 24 }}>
+            <Card title={
+                <div>
+                    <Checkbox style={{ fontSize: 16 }}
+                        value="5"
+                        onChange={systemAllOnChange}
+                        indeterminate={indeterminateSystem}
+                        checked={systemCheckAll}>系统设置</Checkbox>
+                    <span style={{ marginLeft: 16, color: '#B4B4B4' }}>模块的所有功能未开启时，角色操作界面中不显示此模块</span>
+                </div>
+            } style={{ marginTop: 20 }}>
+                <div className={styles.tableCheckGroup}>
+                    <Checkbox.Group onChange={systemOnChange} value={valueSystem}>
+                        <Row gutter={24}>
+                            <Col span={8}><Checkbox value="13" style={{ fontSize: 15, fontStyle: Blob }}>账号管理</Checkbox></Col>
+                            <Col span={8}><Checkbox value="27">查看</Checkbox></Col>
+                            <Col span={8}><Checkbox value="28">管理</Checkbox></Col>
+                        </Row>
+                    </Checkbox.Group>
+                    <div style={{}}>
+                        <span style={{ float: "right" }}>
+                            <Button style={{ marginRight: 16 }}>取消</Button>
+                            <Button type="primary" htmlType="submit">保存</Button>
+                        </span>
+                    </div>
+                </div>
+            </Card>
+
+            {/* <Affix style={{ position: 'absolute', bottom: 0, right: 24 }}>
                 <div style={{}}>
                 <span style={{ float: "right" }}>
                     <Button style={{ marginRight: 16 }}>取消</Button>
                     <Button type="primary" htmlType="submit">保存</Button>
                     </span>
                 </div>
-            </Affix>
+            </Affix> */}
         </div>
     );
 };
